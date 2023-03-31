a70761483f1ecf0708698d627000b09393863a88
AMIRUDDIN MUHAMAD <muhammadamiruddinmuhamad@gmail.com>
2023-03-31 09:08:16 +0800

    login function done

---
+++ index.js
@@ -0,0 +1,37 @@
+let dbUsers = [
+    {
+        username: "amir",
+        password: "password",
+        name: "amiruddin",
+        email: "muhammadamiruddinmuhamad@gmail.com"
+    },
+    {
+        username: "zikry",
+        password: "idk",
+        name: "zikriruddin",
+        email: "zikriruddin@utem.edu.my"
+    },
+]
+
+
+function login(username, password) {
+    console.log("someone try to login with", username, password)
+    let matched = dbUsers.find(element => 
+        element.username == username
+    )
+    if(matched){
+        if(matched.password == password) {
+            return matched
+        }else{
+            return "Password not matched"
+        }
+    
+    }else {
+        return "Username not found"
+    }
+    console.log(matched)
+}
+
+// try to login
+console.log(login ("amir", "password"))
+console.log(login ("amir", "123456"))
